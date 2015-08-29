2fa4ce8c0c0115fb48b78005961fd975c1b9a86a
Yusup Ashrap <awklsgrep@gmail.com>
2015-08-30 00:31:32 +0600

    lighten up

--- emacs.d/personal/custom.el
+++ emacs.d/personal/custom.el
@@ -42,24 +42,24 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" "05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" "83e584d74b0faea99a414a06dae12f11cd3176fdd4eba6674422539951bcfaa8" default)))
+    ("42089e9dfe41a491e48b05c31b6f1851abf4e5a704951bb55cd0e44186937cbb" "67c6ff5060132fa023d8e78a679d4c6573cf1b90c8aef4d1972634956c314da3" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "a444b2e10bedc64e4c7f312a737271f9a2f2542c67caa13b04d525196562bf38" "05c3bc4eb1219953a4f182e10de1f7466d28987f48d647c01f1f0037ff35ab9a" "83e584d74b0faea99a414a06dae12f11cd3176fdd4eba6674422539951bcfaa8" default)))
  '(git-gutter:added-sign " +")
  '(git-gutter:deleted-sign " -")
  '(git-gutter:modified-sign " *")
  '(package-selected-packages
    (quote
-    (window-number ipython elpy powerline color-theme-sanityinc-tomorrow zenburn rotate ac-cider cyberpunk-theme cyberpunk zop-to-char zenburn-theme window-numbering web-mode volatile-highlights vkill undo-tree speed-type smex smartrep smartparens slime rainbow-mode rainbow-delimiters project-explorer ov operate-on-number move-text monokai-theme mediawiki markdown-mode magit-gitflow key-chord json-mode js2-mode ido-ubiquitous htmlize helm-projectile helm-descbinds helm-ag haskell-mode guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine git-gutter gist geiser flycheck flx-ido expand-region exec-path-from-shell elisp-slime-nav easy-kill discover-my-major diminish company-auctex company-anaconda coffee-mode clj-refactor cdlatex browse-kill-ring anzu ace-window))))
+    (gotham-theme aurora-theme window-number ipython elpy powerline color-theme-sanityinc-tomorrow zenburn rotate ac-cider cyberpunk-theme cyberpunk zop-to-char zenburn-theme window-numbering web-mode volatile-highlights vkill undo-tree speed-type smex smartrep smartparens slime rainbow-mode rainbow-delimiters project-explorer ov operate-on-number move-text monokai-theme mediawiki markdown-mode magit-gitflow key-chord json-mode js2-mode ido-ubiquitous htmlize helm-projectile helm-descbinds helm-ag haskell-mode guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine git-gutter gist geiser flycheck flx-ido expand-region exec-path-from-shell elisp-slime-nav easy-kill discover-my-major diminish company-auctex company-anaconda coffee-mode clj-refactor cdlatex browse-kill-ring anzu ace-window))))
 
 ;;ui tweaks
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-(global-hl-line-mode +1)
+(global-hl-line-mode -1)
 
 ;; personal styling
 (set-default-font "Hermit 16")
-(load-theme 'cyberpunk);;https://github.com/tonsky/FiraCodex
+(load-theme 'aurora);;https://github.com/tonsky/FiraCodex
 
 ;(set-face-attribute 'default nil :height 140)
 (scroll-bar-mode -1)
@@ -140,7 +140,7 @@
 (add-hook 'cider-mode-hook #'company-mode)
 
 (setq company-idle-delay 0.3) ; never start completions automatically
-(global-set-key (kbd "M-TAB") #'company-complete) ; use meta+tab, aka C-M-i, as manual trigger
+;(global-set-key (kbd "M-TAB") #'company-complete) ; use meta+tab, aka C-M-i, as manual trigger
 ;(global-set-key (kbd "TAB") #'company-indent-or-complete-common)
 
 (add-hook 'clojure-mode-hook 'cider-mode)
@@ -224,13 +224,13 @@
 
 ;; background color ,modified for monokai
 ;(set-face-foreground 'git-gutter:modified "#282828") monokai default color
-(set-face-foreground 'git-gutter:modified "#1b1d1e")
+;(set-face-foreground 'git-gutter:modified "#1b1d1e")
 (set-face-background 'git-gutter:modified "#777777")
-(set-face-foreground 'git-gutter:added "#1b1d1e")
+(set-face-foreground 'git-gutter:added "#263238")
 (set-face-background 'git-gutter:added "#777777")
-(set-face-foreground 'git-gutter:deleted "#1b1d1e")
+(set-face-foreground 'git-gutter:deleted "#263238")
 (set-face-background 'git-gutter:deleted "#777777")
-
+(set-face-foreground 'git-gutter:modified "#263238")
 
 
 
@@ -331,10 +331,19 @@
   (detabify-buffer)
   (delete-trailing-whitespace))
 
-(defun mbp-clojure-mode-keybindings ()
+(defun nrepl-run-all-tests (ns)
+  (interactive (list (cider-current-ns)))
+  (call-interactively 'cider-switch-to-relevant-repl-buffer)
+  (goto-char (point-max))
+  (insert (format "(cljs.user/run-tests '%s)" ns))
+  (cider-repl-return))
 
-  (local-set-key (kbd "<f1>") 'nrepl-reset)
-  (local-set-key (kbd "<f2> <return>") 'nrepl-refresh))
+(defun mbp-clojure-mode-keybindings ()
+  (local-set-key (kbd "<f7>") 'nrepl-refresh)
+  (local-set-key (kbd "<f5>") 'nrepl-reset)
+  (local-set-key (kbd "<f6>") 'nrepl-run-all-tests))
+
+(add-hook 'clojure-mode-hook 'mbp-clojure-mode-keybindings)
 
 (global-set-key (kbd "C-x t") 'temp-buffer)
 
@@ -412,7 +421,7 @@
 ;;set the background-color of selected region
 
 ;; make a clear selection color
-(set-face-attribute 'region nil :background "#787878")
+;(set-face-attribute 'region nil :background "#787878")
 ;;window management
 ;; highlight the window number in pink color
 (setq magit-auto-revert-mode nil)
@@ -421,18 +430,11 @@
 (setq redisplay-dont-pause t)
 
 ;;I like darkep background
-(set-background-color "#1b1d1e")
-(set-face-background 'fringe "#1b1d1e")
+;(set-background-color "#1b1d1e")
+;(set-face-background 'fringe "#1b1d1e")
 
-(set-face-attribute 'mode-line nil
-                    :foreground "gray60" :background "gray10"
-                    :inverse-video nil
-                    :box '(:line-width 1 :color "gray20" :style nil))
-
-(set-face-attribute 'mode-line-inactive nil
-                    :foreground "gray60" :background "gray10"
-                    :inverse-video nil
-                    :box '(:line-width 1 :color "gray20" :style nil))
+(set-face-attribute 'mode-line nil :foreground "gray60" :background "#324148" :inverse-video nil :box '(:line-width 1 :color "gray20" :style nil))
+(set-face-attribute 'mode-line-inactive nil :foreground "gray60" :background "#324148" :inverse-video nil :box '(:line-width 1 :color "gray20" :style nil))
 
 ;; keep a smooth look of it
 ;(set-face-background 'fringe "#1B1D1E")
@@ -463,12 +465,10 @@
   (if (region-active-p)
       (call-interactively 'elpy-shell-send-region-or-buffer)
     (python-shell-send-string (thing-at-point 'line))))
+
 (define-key elpy-mode-map (kbd "C-c C-c") 'send-line-or-region)
 
 ;; code fucking snippets
+(yas-reload-all)
 (yas-global-mode 1)
-
-(exec-path-from-shell-initialize)
-
-
-(window-numbering-mode +1)
+(add-hook 'clojure-mode-hook #'yas-minor-mode)
