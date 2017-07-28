cd7afe8d09f5369549d06aab55cdeec940a2d63d
Yusup <awklsgrep@gmail.com>
2017-07-28 17:58:01 +0800

    update current network

--- i3/bar.py
+++ i3/bar.py
@@ -1,31 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from network import CurrentNetworkInterface
 from i3pystatus import Status
-from i3pystatus.network import Network, sysfs_interface_up
-from i3pystatus.updates import pacman, cower
-
-
-class MyNetwork(Network):
-    """
-    Modified Network class that automatic switch interface in case of
-    the current interface is down.
-    """
-    on_upscroll = None
-    on_downscroll = None
-
-    def run(self):
-        super().run()
-        if not sysfs_interface_up(self.interface, self.unknown_up):
-            self.cycle_interface()
+from network import CurrentNetworkInterface
 
 status = Status(logfile='/tmp/i3pystatus.log')
-
-status.register("updates",
-    format = "Updates: {count}",
-    format_no_updates = "",
-    on_leftclick="termite --geometry=1200x600 --title=updates -e 'pacaur --needed --noconfirm --noedit -Syu'",
-    backends = [pacman.Pacman(), cower.Cower()])
 
 status.register("clock",
     format=" %H:%M",
@@ -54,7 +32,6 @@ status.register("network",
 status.register("temp")
 
 status.register("cpu_usage",
-    on_leftclick="termite --title=htop -e 'htop'",
     format="  {usage}%",)
 
 status.register("mem",
@@ -67,19 +44,6 @@ status.register("disk",
     path="/home",
     on_leftclick="pcmanfm",
     format=" {avail} GB",)
-
-status.register("text",
-    text="|",
-    color="#222222")
-
-# Shows pulseaudio default sink volume
-#
-# Note: requires libpulseaudio from PyPI
-status.register('ping',
-    format_disabled='',
-    host='dex.top',
-    interval=10,
-    color='#61AEEE')
 
 status.register(CurrentNetworkInterface)
 
--- i3/network.py
+++ i3/network.py
@@ -1,13 +1,21 @@
-from i3pystatus import IntervalModule
 import subprocess
+from i3pystatus import IntervalModule
+
+
+def run_command(cmd):
+    return subprocess.Popen(cmd,shell=True, stdout=subprocess.PIPE).stdout.read().strip().decode('utf-8')
 
 def current_network_interface():
     default_ni = 'down'
     command = "nmcli device status |grep -E -v 'disconnected|docker|bridge' |grep connected |awk '{print $4}'"
-    cmd_ni = subprocess.Popen(command,shell=True, stdout=subprocess.PIPE).stdout.read().strip().decode('utf-8')
+    cmd_ni = run_command(command)
     if not cmd_ni:
         return default_ni
     return cmd_ni
+
+def connect_to_network():
+    command = "nmcli c up bitmain-download"
+    run_command(command)
 
 class CurrentNetworkInterface(IntervalModule):
     """
@@ -26,6 +34,7 @@ class CurrentNetworkInterface(IntervalModule):
     warn_color = "#FFFF00"
     alert_color = "#FF0000"
     round_size = 1
+    down_time = 0
 
     settings = (
         ("format", "format string used for output."),
@@ -44,15 +53,23 @@ class CurrentNetworkInterface(IntervalModule):
 
     def run(self):
         ni = current_network_interface()
-
+        result = ni
         if ni == 'down':
             color = self.alert_color
+            self.down_time = self.down_time + 1
+            if self.down_time == 8:
+                connect_to_network()
         else:
+            self.down_time = 0
             color = self.color
 
+        if self.down_time != 0:
+            result = ni + "," + str(self.down_time)
+
         cdict = {
-            "current_network_interface": ni,
+            "current_network_interface": " " + result,
         }
+
         self.data = cdict
         self.output = {
             "full_text": self.format.format(**cdict),
